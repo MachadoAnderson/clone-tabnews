fbf9e6143e8ae8ef35f8506f4f37c0f81f4e15b5
MachadoAnderson <MachadoAnderson@users.noreply.github.com>
2024-03-14 20:15:46 +0000

    Ajustando a frase da Index.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Por você e pela nossa bebê papai está codando para todo o mundo ver! ;) </h1>
+    return <h1>Linda, é por você e pela nossa bebê papai está codando para todo o mundo ver! ;) </h1>
 }
 
 export default Home;
